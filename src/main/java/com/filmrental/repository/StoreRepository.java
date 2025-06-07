04777ff993b4819116690e39dea3f499230199fe
Ayushi0349 <ayushiraturi004@gmail.com>
2025-06-07 15:50:59 +0530

    [Ayushi] feat : Change StoreRepository

--- src/main/java/com/filmrental/repository/StoreRepository.java
+++ src/main/java/com/filmrental/repository/StoreRepository.java
@@ -5,5 +5,6 @@ import com.filmrental.model.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer> {}
