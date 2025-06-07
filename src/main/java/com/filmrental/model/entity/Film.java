9ff370e8d42974f3148cff5a6de0f2136a9b6264
ankiitt-07 <ak077636@gmail.com>
2025-06-07 14:31:15 +0530

    [Ankit] feat: add(Repository,Controller,Model,Mapper)

---
+++ src/main/java/com/filmrental/model/entity/Film.java
@@ -0,0 +1,67 @@
+
+package com.filmrental.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.Year;
+
+
+@Entity
+@Table(name = "film")
+@Data
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "film_id")
+    private Integer filmId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "release_year")
+    private Year releaseYear;
+
+    @ManyToOne
+    @JoinColumn(name = "language_id", nullable = false)
+    private Language language;
+
+    @ManyToOne
+    @JoinColumn(name = "original_language_id")
+    private Language originalLanguage;
+
+    @Column(name = "rental_duration", nullable = false)
+    private Integer rentalDuration;
+
+    @Column(name = "rental_rate", nullable = false)
+    private BigDecimal rentalRate;
+
+    @Column(name = "length")
+    private Integer length;
+
+    @Column(name = "replacement_cost", nullable = false)
+    private BigDecimal replacementCost;
+
+    @Column(name = "rating")
+    private String rating;
+
+    @Column(name = "special_features")
+    private String specialFeatures;
+
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
+
+    @OneToMany(mappedBy = "film")
+    private List<FilmActor> filmActors;
+
+    @OneToMany(mappedBy = "film")
+    private List<FilmCategory> filmCategories;
+
+    @OneToMany(mappedBy = "film")
+    private List<Inventory> inventories;
+}
