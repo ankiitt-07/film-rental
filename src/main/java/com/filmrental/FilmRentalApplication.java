a1a9d074bc1b6b2e0cbffc6fa80a1afbd55dc07c
simrandhiman <simrandhiman3121@gmail.com>
2025-06-14 15:07:16 +0530

    Feat:BackEnd

--- src/main/java/com/filmrental/FilmRentalApplication.java
+++ src/main/java/com/filmrental/FilmRentalApplication.java
@@ -1,13 +1,16 @@
 package com.filmrental;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FilmRentalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FilmRentalApplication.class, args);
+        log.info("Project Started BackEnd ");
     }
 
 }
